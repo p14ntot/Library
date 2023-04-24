3b42729ab39a129ed80fa33b6f24772aa8ea8e60
p14ntot <p14ntot@ionio.gr>
2023-04-24 11:54:44 +0300

    Addo some style in form and btns

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function DisplayLibrary(library){
     for(let i=0; i<library.length; i++){
         const div=document.createElement('div')
         div.classList.add('book');
+        div.style.border = "1px solid black"; // creates border for each book-div
         for(var j in library[i]){
             const info=document.createElement('div')
             info.textContent=j+':'+library[i][j];
@@ -68,7 +69,7 @@ addbtn.addEventListener('click',()=>{
     document.body.removeChild(allDivs[i]);
     }
 
-    const formDiv = document.getElementById('form-div');
+    //const formDiv = document.getElementById('form-div');
     
     const newForm = document.createElement('form');
 
@@ -76,18 +77,21 @@ addbtn.addEventListener('click',()=>{
     newTitle.setAttribute('type','text');
     newTitle.setAttribute('name', 'title');
     newTitle.setAttribute('placeholder','title');
+    newTitle.style.height = '30px';
     newForm.appendChild(newTitle);
 
     const newAuthor=document.createElement('input');
     newAuthor.setAttribute('type','text');
     newAuthor.setAttribute('name', 'author');
     newAuthor.setAttribute('placeholder','Author');
+    newAuthor.style.height = '30px';
     newForm.appendChild(newAuthor);
 
     const newPages=document.createElement('input');
     newPages.setAttribute('type','number');
     newPages.setAttribute('name', 'pages');
     newPages.setAttribute('placeholder','Pages');
+    newPages.style.height = '30px';
     newForm.appendChild(newPages);
 
 
@@ -95,20 +99,41 @@ addbtn.addEventListener('click',()=>{
     newRead.setAttribute('type','text');
     newRead.setAttribute('name', 'read');
     newRead.setAttribute('placeholder','Have you read this book?');
+    newRead.style.height = '30px';
     newForm.appendChild(newRead);
 
 
+    
+    //create submit btn
     const subbmitBtn=document.createElement('button');
     subbmitBtn.setAttribute('type','submit');
     subbmitBtn.setAttribute('name', 'submit');
     subbmitBtn.textContent = 'Submit';
 
-    subbmitBtn.style.width = '150px'; 
+    
+    // style the submit button
+    subbmitBtn.style.width = '120px'; 
     subbmitBtn.style.height = '50px';
+    subbmitBtn.style.backgroundColor = '#3E51FC';
+    subbmitBtn.style.color = 'white';
+    subbmitBtn.style.fontWeight = 'bolder';
+    subbmitBtn.style.fontSize = '1.3em';
+    subbmitBtn.style.borderRadius='11px';
     
     newForm.appendChild(subbmitBtn);
 
+    newForm.style.border='solid #AEFFFB 2px';
+    newForm.style.display='flex';
+    newForm.style.flexDirection='column';
+    newForm.style.width='250px';
+    newForm.style.height='400px';
+    newForm.style.marginTop='80px';
+    newForm.style.marginLeft='40%';
+    newForm.style.justifyContent = 'space-between';
+    
+
     document.body.appendChild(newForm);
+    
 
 })
 
